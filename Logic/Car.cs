c2821232352d6d3728650245e486ec7b2dbb8784
Keko <sergiograciacorreia@gmail.com>
2025-02-13 11:29:12 +0100

    Agregar archivos de proyecto.

---
+++ Logic/Car.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Driving_Sim_WPF.Logic
+{
+	internal class Car
+	{
+	}
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Driving_Sim_WPF
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void btnMotorOn_Click(object sender, RoutedEventArgs e)
+		{
+			// Solo se imprime un mensaje para previsualizar el evento
+			Console.WriteLine("Botón Motor ON presionado");
+		}
+
+		private void btnMotorOff_Click(object sender, RoutedEventArgs e)
+		{
+			// Solo se imprime un mensaje para previsualizar el evento
+			Console.WriteLine("Botón Motor OFF presionado");
+		}
+
+		private void btnAccelerate_Click(object sender, RoutedEventArgs e)
+		{
+			// Solo se imprime un mensaje para previsualizar el evento
+			Console.WriteLine("Botón Acelerar presionado");
+		}
+
+		private void btnBrake_Click(object sender, RoutedEventArgs e)
+		{
+			// Solo se imprime un mensaje para previsualizar el evento
+			Console.WriteLine("Botón Frenar presionado");
+		}
+	}
+}
